b73bbb36693add5ee4a078c62538bcfa635ab0f5
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2021-02-25 02:42:04 -0600

    chore: fixed ci

--- tests/_sequelize.js
+++ tests/_sequelize.js
@@ -3,7 +3,10 @@ const JSONSchemaSequelizer = require('..');
 module.exports.setup = (options, refs, cwd) => {
   const config = {
     username: process.env.CI ? 'postgres' : process.env.LOGNAME,
-    database: process.env.CI ? 'travis_ci_test' : 'test',
+    password: process.env.CI ? 'postgres' : undefined,
+    database: process.env.CI ? 'ci_db_test' : 'test',
+    host: process.env.CI ? process.env.POSTGRES_HOST : '0.0.0.0',
+    port: process.env.CI ? process.env.POSTGRES_PORT : 5432,
     dialect: options.dialect,
     storage: options.storage,
     logging: options.logging || false,
